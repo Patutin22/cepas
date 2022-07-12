e388b0becc98491f3efa85ab9a5cbce24446198a
Gladys <yasha_2921@hotmail.com>
2022-07-11 20:31:49 -0400

    Agrega informacion del seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :blends
   resources :strains
   resources :wines
+
+  root 'wines#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
